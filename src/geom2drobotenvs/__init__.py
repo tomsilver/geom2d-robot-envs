78c500e519c228c9ccee394e4d0578c517d0bfdb
Tom Silver <tomssilver@gmail.com>
2025-07-23 08:14:11 -0400

    switch to agg backend

--- src/geom2drobotenvs/__init__.py
+++ src/geom2drobotenvs/__init__.py
@@ -1,6 +1,12 @@
 """Register environments for use with gym.make()."""
 
+import matplotlib
 from gymnasium.envs.registration import register
+
+# We require the Agg backend to ensure consistent image shapes between systems.
+# Previously we did not have this, and different users with the macosx had different
+# rendered image shapes because they had different monitor sizes.
+matplotlib.use("Agg")
 
 
 def register_all_environments() -> None:
